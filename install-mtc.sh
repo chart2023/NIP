573bb4a9228045a71a7e574354c800d79c8c44a1
Apichart <chart2023@gmail.com>
2016-02-10 12:43:57 +0700

    Update install-mtc.sh

--- install-mtc.sh
+++ install-mtc.sh
@@ -5,7 +5,7 @@ sudo apt-get install gcc libssl-dev g++ make -y
 wget -qO - http://nodejs.org/dist/v0.10.22/node-v0.10.22.tar.gz | tar -xz
 cd node-v0.10.22
 ./configure && make && sudo make install
-expect install-mtc.exp
+expect /opt/openbaton/scripts/install-mtc.exp
 cd OpenMTC-Chula
 make setup
 npm install winston
