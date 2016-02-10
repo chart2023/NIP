0f4121ff0de73ef7782aee614edd7a66cd764a81
Apichart <chart2023@gmail.com>
2016-02-10 12:18:47 +0700

    Update install-mtc.sh

--- install-mtc.sh
+++ install-mtc.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install mongodb make subversion -y
+sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make -y
 wget -qO - http://nodejs.org/dist/v0.10.22/node-v0.10.22.tar.gz | tar -xz
 cd node-v0.10.22
 ./configure && make && sudo make install
+expect install-mtc.exp
+cd OpenMTC-Chula
+make setup
+npm install winston
