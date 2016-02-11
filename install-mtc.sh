b23fd832bc52bb53312aad2e93100e0fb5079de0
Apichart <chart2023@gmail.com>
2016-02-11 12:29:23 +0700

    update
    update

--- install-mtc.sh
+++ install-mtc.sh
@@ -4,7 +4,7 @@ log_file="/home/ubuntu/install-log.txt"
 exec 1>> $log_file 2>&1
 sudo apt-get update
 sudo apt-get install language-pack-en-base
-sudo locale-gen UTF-8
+sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make -y
 wget -qO - http://nodejs.org/dist/v0.10.9/node-v0.10.9.tar.gz | tar -xz
@@ -15,4 +15,4 @@ expect /opt/openbaton/scripts/install-mtc.exp
 cd OpenMTC-Chula
 sudo make setup
 npm install winston
-npm install log4js
+#npm install log4js
