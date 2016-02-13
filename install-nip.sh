884aa62c5e207686afe095f279c301f18fb510b4
Apichart <chart2023@gmail.com>
2016-02-13 11:54:32 +0700

    Update install-nip.sh

--- install-nip.sh
+++ install-nip.sh
@@ -2,8 +2,8 @@
 log_file="/home/ubuntu/installnip-log.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
-sudo mkdir /OpenMTC-Chula/openmtc-NIP/
-sudo git clone https://github.com/chart2023/ProxyGateway.git /OpenMTC-Chula/openmtc-NIP/
+sudo mkdir /OpenMTC-Chula/openmtc-NIP/ProxyGateway
+sudo git clone https://github.com/chart2023/ProxyGateway.git /OpenMTC-Chula/openmtc-NIP/ProxyGateway/
 sudo cp /OpenMTC-Chula/openmtc-NIP/ProxyGateway/config-nscl.js /OpenMTC-Chula/openmtc/settings/
 sudo cp /OpenMTC-Chula/openmtc-NIP/ProxyGateway/config-gscl.js /OpenMTC-Chula/openmtc/settings/
 sudo mkdir /OpenMTC-Chula/openmtc-NIP/lib/NIP
