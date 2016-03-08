683626f06d0a9b3f34e088dea3ee3e4f0dfc4577
Apichart <chart2023@gmail.com>
2016-03-08 14:43:45 +0700

    Update install-mtc.sh

--- install-mtc.sh
+++ install-mtc.sh
@@ -2,7 +2,7 @@
 log_file="/home/ubuntu/install-log.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install language-pack-en-base -y
 sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
