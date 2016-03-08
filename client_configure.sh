f62f68b12dd7c56ad41b2ebaa35e94b14b9a9346
Apichart <chart2023@gmail.com>
2016-03-08 17:06:40 +0700

    Update client_configure.sh

--- client_configure.sh
+++ client_configure.sh
@@ -2,6 +2,6 @@
 #sudo touch /opt/openbaton/clientinfo.txt
 #sudo bash -c 'echo $client_private > /opt/openbaton/clientinfo.txt'
 #sudo bash -c 'echo $client_private_floatingIp >> /opt/openbaton/clientinfo.txt'
-sudo echo $server_private >> /home/ubuntu/serverinfo.txt
-sudo echo $server_private_floatingIp >> /home/ubuntu/serverinfo.txt
+sudo echo $client_private >> /home/ubuntu/serverinfo.txt
+sudo echo $client_private_floatingIp >> /home/ubuntu/serverinfo.txt
 #sudo echo $server_hostname >> /home/ubuntu/serverinfo.txt
