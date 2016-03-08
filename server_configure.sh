73d7eed30cdb2e5e5fa7721067a340e4ef377a81
Apichart <chart2023@gmail.com>
2016-03-08 23:35:57 +0700

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -4,4 +4,4 @@
 #sudo bash -c 'echo $server_private_floatingIp >> /opt/openbaton/test.txt'
 sudo echo $server_private >> /home/ubuntu/clientinfo.txt
 sudo echo $server_private_floatingIp >> /home/ubuntu/clientinfo.txt
-sudo echo exports.iplocal=$server_private >> /OpenMTC-Chula/openmtc/settings/ipnscl.js
+sudo echo exports.iplocal=\'$server_private\'\; >> /OpenMTC-Chula/openmtc/settings/ipnscl.js
