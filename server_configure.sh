fc803ece88a69f912486cfde4db7801a49aab5a2
Apichart <chart2023@gmail.com>
2016-03-08 17:07:32 +0700

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo touch /opt/openbaton/test.txt
-sudo bash -c 'echo $server_private > /opt/openbaton/test.txt'
-sudo bash -c 'echo $server_private_floatingIp >> /opt/openbaton/test.txt'
+#sudo touch /opt/openbaton/test.txt
+#sudo bash -c 'echo $server_private > /opt/openbaton/test.txt'
+#sudo bash -c 'echo $server_private_floatingIp >> /opt/openbaton/test.txt'
+sudo echo $server_private >> /home/ubuntu/clientinfo.txt
+sudo echo $server_private_floatingIp >> /home/ubuntu/clientinfo.txt
