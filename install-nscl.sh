df34ae51d09442ec0b396b75ef9da6fb0bf7696c
Apichart <chart2023@gmail.com>
2016-04-27 17:16:16 +0700

    Update install-nscl.sh

--- install-nscl.sh
+++ install-nscl.sh
@@ -7,10 +7,10 @@ sudo apt-get install language-pack-en-base -y
 sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make -y
-wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_ab1d7ed610574164b21c22f9b542998a/openmtc/node-v0.10.42.tar.gz -o /home/ubuntu/ | tar -xz
+wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_ab1d7ed610574164b21c22f9b542998a/openmtc/node-v0.10.42.tar.gz -o /home/ubuntu/nodejs.tar.gz | tar -xz
 cd /home/ubuntu/node-v0.10.42/
 ./configure && make && sudo make install
 cd ..
 sudo npm -g install npm@2.7.6
-wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ab1d7ed610574164b21c22f9b542998a/openmtc/OpenMTC-nscl.zip -O /home/ubuntu/
-unzip OpenMTC-nscl.zip
+wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ab1d7ed610574164b21c22f9b542998a/openmtc/OpenMTC-nscl.zip -o /home/ubuntu/OpenMTC-nscl.zip
+unzip /home/ubuntu/OpenMTC-nscl.zip
