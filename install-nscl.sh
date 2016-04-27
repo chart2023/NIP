bc305e7cefb843062dda04dc7a38bc4b40c40507
Apichart <chart2023@gmail.com>
2016-04-27 18:14:26 +0700

    Update install-nscl.sh

--- install-nscl.sh
+++ install-nscl.sh
@@ -12,5 +12,5 @@ cd /node-v0.10.42/
 ./configure && make && sudo make install
 cd ..
 sudo npm -g install npm@2.7.6
-wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ab1d7ed610574164b21c22f9b542998a/openmtc/OpenMTC-nscl.zip -o /home/ubuntu/OpenMTC-nscl.zip
+wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ab1d7ed610574164b21c22f9b542998a/openmtc/OpenMTC-nscl.zip -O /home/ubuntu/OpenMTC-nscl.zip
 unzip /home/ubuntu/OpenMTC-nscl.zip
