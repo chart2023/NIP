c9f73f9146cb0b3ebc47f2e6eb6757231932b01a
Apichart <chart2023@gmail.com>
2016-05-03 17:31:54 +0700

    Update start-real-time.sh

--- start-real-time.sh
+++ start-real-time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sleep 30
-sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_IEEE1888_ETSI.js &
+sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_IEEE1888_ETSI.js >/home/ubuntu/ieee.log 2>/home/ubuntu/ieee.err &
 sleep 10
-sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_ETSI_IEEE1888_nscl.js &
+sudo nohup sudo node /OpenMTC-Chula/openmtc-NIP/ProxyGateway/NIP_ETSI_IEEE1888_nscl.js >/home/ubuntu/etsi.log 2>/home/ubuntu/etsi.err &
