356249835867d6bf9a547ff621b87cda61274e60
Apichart <chart2023@gmail.com>
2016-05-06 00:40:21 +0700

    Update install-nodejs.sh

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -2,9 +2,11 @@
 log_file="/home/ubuntu/install-node.txt"
 [ -f "$log_file" ] || touch "$log_file"
 exec 1>> $log_file 2>&1
-wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/node-v0.10.42.tar.gz | tar -xz
-cd /node-v0.10.42/
-./configure && make && sudo make install
-cd ..
+#wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/node-v0.10.42.tar.gz | tar -xz
+#cd /node-v0.10.42/
+#./configure && make && sudo make install
+#cd ..
+sudo apt-get install nodejs
 sudo apt-get install -y build-essential
+sudo apt-get install npm
 sudo npm -g install npm@2.7.6
