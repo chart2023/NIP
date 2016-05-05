657918c02b087e04903ab0cf5b8497d32dac76fb
Apichart <chart2023@gmail.com>
2016-05-05 14:14:32 +0700

    Update install-nip.sh

--- install-nip.sh
+++ install-nip.sh
@@ -8,11 +8,10 @@ sudo apt-get install language-pack-en-base -y
 sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make unzip -y
-#wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/node-v0.10.42.tar.gz | tar -xz
-#cd /node-v0.10.42/
-#./configure && make && sudo make install
-#cd ..
-sudo apt-get install -y nodejs
+wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/node-v0.10.42.tar.gz | tar -xz
+cd /node-v0.10.42/
+./configure && make && sudo make install
+cd ..
 sudo apt-get install -y build-essential
 sudo npm -g install npm@2.7.6
 wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/OpenMTC-nip.zip -O /home/ubuntu/OpenMTC-nip.zip
