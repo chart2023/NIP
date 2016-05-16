5c0157b5852920ed9f686b79e223c3953ee6bbe6
Apichart <chart2023@gmail.com>
2016-05-16 14:05:57 +0700

    Update install-openmtc-nscl.sh

--- install-openmtc-nscl.sh
+++ install-openmtc-nscl.sh
@@ -8,7 +8,7 @@ sudo apt-get install language-pack-en-base -y
 sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make unzip -y
-wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/OpenMTC-nscl.zip -O /home/ubuntu/OpenMTC-nscl.zip
+wget --tries=10 http://192.168.9.11:8080/v1/AUTH_e619f03c8bbb4873998c5c040a98f104/openmtc/OpenMTC-nscl.zip -O /home/ubuntu/OpenMTC-nscl.zip
 unzip /home/ubuntu/OpenMTC-nscl.zip
 sudo cp /opt/openbaton/scripts/start-nscl.sh /etc/init.d/start-nscl.sh
 sudo chmod ugo+x /etc/init.d/start-nscl.sh
