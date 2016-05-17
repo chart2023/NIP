cf277410350b8b8e0cfd3facedcec1df0f027b5d
Apichart <chart2023@gmail.com>
2016-05-17 18:09:22 +0700

    Update install-openmtc-nip.sh

--- install-openmtc-nip.sh
+++ install-openmtc-nip.sh
@@ -8,7 +8,7 @@ sudo apt-get install language-pack-en-base -y
 sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make unzip -y
-wget --tries=10 http://192.168.9.11:8080/v1/AUTH_e619f03c8bbb4873998c5c040a98f104/openmtc/OpenMTC-nip.zip -O /home/ubuntu/OpenMTC-nip.zip
+wget --tries=10 http://192.168.9.14:8080/v1/AUTH_d73b59ff5bd44abcb5f30eafe2aaf149/openmtc/OpenMTC-nip.zip -O /home/ubuntu/OpenMTC-nip.zip
 unzip /home/ubuntu/OpenMTC-nip.zip
 sudo cp /opt/openbaton/scripts/start-real-time.sh /etc/init.d/start-real-time.sh
 sudo chmod ugo+x /etc/init.d/start-real-time.sh
