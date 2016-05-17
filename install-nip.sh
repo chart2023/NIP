4ea9d55b170d4e3daa6fb6f52d183955f8d9df5b
Apichart <chart2023@gmail.com>
2016-05-17 18:05:20 +0700

    Update install-nip.sh

--- install-nip.sh
+++ install-nip.sh
@@ -8,13 +8,13 @@ sudo apt-get install language-pack-en-base -y
 sudo locale-gen en_US en_US.UTF-8 cy_GB.UTF-8
 sudo apt-get install mongodb make subversion expect -y
 sudo apt-get install gcc libssl-dev g++ make unzip -y
-wget --tries=10 -O - http://192.168.9.11:8080/v1/AUTH_e619f03c8bbb4873998c5c040a98f104/openmtc/node-v0.10.42.tar.gz | tar -xz
+wget --tries=10 -O - http://192.168.9.14:8080/v1/AUTH_d73b59ff5bd44abcb5f30eafe2aaf149/openmtc/node-v0.10.42.tar.gz | tar -xz
 cd /node-v0.10.42/
 ./configure && make && sudo make install
 cd ..
 sudo apt-get install -y build-essential
 sudo npm -g install npm@2.7.6
-wget --tries=10 http://192.168.9.11:8080/v1/AUTH_ee10034eae5a462daa29e6f9f2738e8e/openmtc/OpenMTC-nip.zip -O /home/ubuntu/OpenMTC-nip.zip
+wget --tries=10 http://192.168.9.14:8080/v1/AUTH_d73b59ff5bd44abcb5f30eafe2aaf149/openmtc/OpenMTC-nip.zip -O /home/ubuntu/OpenMTC-nip.zip
 unzip /home/ubuntu/OpenMTC-nip.zip
 sudo cp /opt/openbaton/scripts/start-real-time.sh /etc/init.d/start-real-time.sh
 sudo chmod ugo+x /etc/init.d/start-real-time.sh
