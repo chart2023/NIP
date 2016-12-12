b3914b4202622d65ac4e27947be8300c6f19ed08
Apichart <chart2023@users.noreply.github.com>
2016-12-12 09:41:07 +0700

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#sudo touch /opt/openbaton/test.txt
-#sudo bash -c 'echo $server_private > /opt/openbaton/test.txt'
-#sudo bash -c 'echo $server_private_floatingIp >> /opt/openbaton/test.txt'
-sudo echo $server_private >> /home/ubuntu/clientinfo.txt
-sudo echo $server_private_floatingIp >> /home/ubuntu/clientinfo.txt
-sudo echo exports.ipnscl=\'$server_private\'\; >> /OpenMTC-Chula/openmtc/settings/ipserv.js
-sudo bash -c "echo exports.ipnip=\'`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`\'\; >> /OpenMTC-Chula/openmtc/settings/ipserv.js"
-sudo echo exports.fipnip=\'$server_private_floatingIp\'\; >> /OpenMTC-Chula/openmtc/settings/ipserv.js
-sudo echo exports.ipopenstack=\'161.200.90.78\'\; >> /OpenMTC-Chula/openmtc/settings/ipserv.js
+sudo rm -rf ${HOME}/nscl_info.conf
+echo "nscl_ip=$nscl_private" | tee --append ${HOME}/nscl_info.conf
+echo "nscl_fip=$nscl_private_floatingIp" | tee --append ${HOME}/nscl_info.conf
+echo "nscl_hostname=$nscl_hostname" | tee --append ${HOME}/nscl_info.conf
+sudo rm -rf ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+#SET IP LBaaS
+echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+#echo "exports.iplbaas='10.0.14.7';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+#Set IP NSCL
+#echo "exports.ipnscl='$nscl_private';" | tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+sudo bash -c "echo exports.ipnip=\'`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`\'\; >> ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js"
+echo "exports.fipnip='$nscl_private_floatingIp';" >> tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
+echo "exports.ipopenstack='192.168.9.14';" >> tee --append ${HOME}/OpenMTC-Chula/openmtc/settings/ipserv.js
