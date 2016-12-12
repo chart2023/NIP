29e1c4c03505ea202f4a5365bc7eee84ccbce7c9
Apichart <chart2023@users.noreply.github.com>
2016-12-12 10:48:39 +0700

    Delete db_configure.sh

--- db_configure.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "db_ip=$db_private" | sudo tee --append ${HOME}/db_info.conf
-echo "db_fip=$db_private_floatingIp" | sudo tee --append ${HOME}/db_info.conf
-echo "db_hostname=$db_hostname" | sudo tee --append ${HOME}/db_info.conf
